ae69ad4e5c1a72def120a8d476c350676f179900
xkaoru-hanayamax <mizusaki1127@icloud.com>
2026-02-08 23:10:54 +0900

    エラー対応

--- src/financial/__init__.py
+++ src/financial/__init__.py
@@ -1,6 +1,7 @@
 """財務分析モジュール"""
 from .loader import load_financial_data, get_company_data
 from .metrics import calculate_metrics, format_metrics_for_llm, format_raw_data_as_markdown
+from .analyzer import FinancialAnalyzer, summarize_with_llm
 
 __all__ = [
     "load_financial_data",
@@ -8,4 +9,6 @@ __all__ = [
     "calculate_metrics",
     "format_metrics_for_llm",
     "format_raw_data_as_markdown",
+    "FinancialAnalyzer",
+    "summarize_with_llm",
 ]
